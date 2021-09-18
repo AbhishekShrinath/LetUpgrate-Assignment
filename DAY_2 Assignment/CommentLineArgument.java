93116bce6975b598dae951b5e83cc546090bb42e
AbhishekShrinath <80875523+AbhishekShrinath@users.noreply.github.com>
2021-09-18 15:10:15 +0530

    Add files via upload

---
+++ DAY_2 Assignment/CommentLineArgument.java
@@ -0,0 +1,12 @@
+public class CommentLineArgument
+ {
+
+public static void main(String args[])
+ {
+
+	int i=Integer.parseInt(args[0]);
+	int j=Integer.parseInt(args[1]);
+	int sum=i+j;
+	System.out.println("Sum of Two Number is "+sum);
+}
+}
